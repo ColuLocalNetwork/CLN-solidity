63be0c2e18fa6897770c8b2715188e645c41fc8a
Lior Rabin <lior@colu.com>
2017-11-14 11:57:59 +0200

    solc version 0.4.18

--- scripts/deployment/getMultiSigWithDailyLimitBytecode.js
+++ scripts/deployment/getMultiSigWithDailyLimitBytecode.js
@@ -1,6 +1,4 @@
 var config = require('./config')
-var Web3 = require('web3')
-var web3 = new Web3(new Web3.providers.HttpProvider(config.get('web3Provider')))
 var solc = require('solc')
 var abi = require('ethereumjs-abi')
 var fs = require('fs')
