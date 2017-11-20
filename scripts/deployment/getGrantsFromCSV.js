960768ac2acb0553577df93a83856cc3369fa669
Lior Rabin <lior@colu.com>
2017-11-20 13:47:08 +0200

    get grants from csv

---
+++ scripts/deployment/getGrantsFromCSV.js
@@ -0,0 +1,58 @@
+const config = require(__dirname + '/config')
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider(config.get('web3Provider')))
+const fs = require('fs')
+const parse = require('csv-parse')
+const async = require('async')
+const argv = require('yargs').argv
+
+const inputFile = __dirname + '/config/grants.csv'
+
+const MINUTE = 60
+const HOUR = 60 * MINUTE
+const DAY = 24 * HOUR
+const MONTH = 30 * DAY
+const YEAR = 12 * MONTH
+
+const grants = []
+
+async.auto({
+	getBlockNumber: web3.eth.getBlockNumber,
+	getBlock: ['getBlockNumber', (results, cb) => {
+		web3.eth.getBlock(results.getBlockNumber, cb)
+	}],
+	grants: ['getBlock', (results, cb) => {
+		const now = results.getBlock.timestamp
+		const parser = parse({delimiter: ', '}, (err, data) => {
+			if(err) {
+				return console.error('err = ', err)
+			}
+			async.eachSeries(data, (line, cb) => {
+				grants.push({
+					to: line[0],
+					value: line[1],
+		            start: argv.start || now,
+		            cliff: argv.cliff || (6*MONTH),
+		            end: argv.end || (now + 3*YEAR),
+		            installmentLength: argv.installmentLength || (1*MONTH),
+		            revokable: argv.revokable || true
+				})
+				cb()
+			}, function() {
+				let formattedGrants = grants.map(grant => {
+					return Object.values(grant).join(',')
+				})
+
+				fs.writeFileSync(__dirname + '/formattedGrants' + now + '.csv', formattedGrants.join('\n'), 'utf8')
+			})
+		})
+		fs.createReadStream(inputFile).pipe(parser)
+		cb()
+	}]
+}, (err, results) => {
+	if (err) {
+		return console.error('err =', err)
+	}
+	console.log('DONE')
+})
+
