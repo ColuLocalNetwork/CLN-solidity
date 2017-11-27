54ada2b80c1684d35ea5de512eae71f0092c880d
Lior Rabin <lior@colu.com>
2017-11-27 12:51:45 +0200

    unify contracts & get bytecode from unified code

--- scripts/deployment/unifyContracts.js
+++ scripts/deployment/unifyContracts.js
@@ -15,20 +15,20 @@ var contracts = [
 ]
 
 var now = +new Date()
-var solidityVersion = config.get('solidity').replace('pragma solidity', '').replace(';', '')
+var solidityVersion = config.get('solidity').replace('pragma solidity ', '').replace(';', '')
 var compilerVersion = config.get('compilerVersion')
-var unifiedContract = __dirname + '/Unified_' + solidityVersion + '_' + compilerVersion + '_' + now + '.sol'
+var filePath = __dirname + '/Unified_' + solidityVersion + '_' + compilerVersion + '_' + now + '.sol'
 
 unifyContracts(err => {
 	if(err) return console.error('err =', err)
-	console.log('DONE')
+	console.log('unifyContracts done')
 })
 
-function unifyContracts(callback) {
+function unifyContracts(cb) {
 	async.waterfall([
 		function writeFileWithHeader (callback) {
 			// open file for writing, file is created (if it does not exist) or truncated (if it exists)
-			fs.writeFile(unifiedContract, config.get('solidity'), {flag: 'w'}, callback)
+			fs.writeFile(filePath, config.get('solidity'), {flag: 'w'}, callback)
 		},
 		function readFiles (callback) {
 			async.concat(contracts, readFile, callback)
@@ -39,7 +39,9 @@ function unifyContracts(callback) {
 		function appendToUnified (clearedFiles, callback) {
 			async.eachSeries(clearedFiles, appendToFile, callback)
 		},
-	], callback)
+	], err => {
+		cb(err, filePath)
+	})
 
 	function readFile(file, cb) {
 		fs.readFile(__dirname + '/../../contracts/' + file, 'utf8', cb)
@@ -54,7 +56,7 @@ function unifyContracts(callback) {
 	}
 
 	function appendToFile(file, cb) {
-		fs.appendFile(unifiedContract, file, cb)
+		fs.appendFile(filePath, file, cb)
 	}
 }
 
