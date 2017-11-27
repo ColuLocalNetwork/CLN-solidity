0fabcb3b93f38c0ada4a18e4b38da667559410c0
Lior Rabin <lior@colu.com>
2017-11-27 13:02:48 +0200

    minor

--- scripts/deployment/getColuLocalNetworkSaleAbi.js
+++ scripts/deployment/getColuLocalNetworkSaleAbi.js
@@ -3,12 +3,12 @@ var solc = require('solc')
 var fs = require('fs')
 
 var input = {
-  'BasicToken.sol': fs.readFileSync(__dirname + '/../../contracts/BasicToken.sol', 'utf8'),
-  'ERC20.sol': fs.readFileSync(__dirname + '/../../contracts/ERC20.sol', 'utf8'),
   'Ownable.sol': fs.readFileSync(__dirname + '/../../contracts/Ownable.sol', 'utf8'),
   'SafeMath.sol': fs.readFileSync(__dirname + '/../../contracts/SafeMath.sol', 'utf8'),
-  'ColuLocalNetwork.sol': fs.readFileSync(__dirname + '/../../contracts/ColuLocalNetwork.sol', 'utf8'),
+  'ERC20.sol': fs.readFileSync(__dirname + '/../../contracts/ERC20.sol', 'utf8'),
+  'BasicToken.sol': fs.readFileSync(__dirname + '/../../contracts/BasicToken.sol', 'utf8'),
   'TokenHolder.sol': fs.readFileSync(__dirname + '/../../contracts/TokenHolder.sol', 'utf8'),
+  'ColuLocalNetwork.sol': fs.readFileSync(__dirname + '/../../contracts/ColuLocalNetwork.sol', 'utf8'),
   'VestingTrustee.sol': fs.readFileSync(__dirname + '/../../contracts/VestingTrustee.sol', 'utf8'),
   'ColuLocalNetworkSale.sol': fs.readFileSync(__dirname + '/../../contracts/ColuLocalNetworkSale.sol', 'utf8'),
 }
