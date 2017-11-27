392a04844c0bb177ecb1a06e8628fcdac312c1d7
Lior Rabin <lior@colu.com>
2017-11-27 12:07:42 +0200

    make unifyContracts export a function with callback

--- scripts/deployment/unifyContracts.js
+++ scripts/deployment/unifyContracts.js
@@ -14,39 +14,50 @@ var contracts = [
 	'ColuLocalNetworkSale.sol',
 ]
 
-var unifiedContract = __dirname + '/Unified.sol'
+var now = +new Date()
+var solidityVersion = config.get('solidity').replace('pragma solidity', '').replace(';', '')
+var compilerVersion = config.get('compilerVersion')
+var unifiedContract = __dirname + '/Unified_' + solidityVersion + '_' + compilerVersion + '_' + now + '.sol'
 
-async.waterfall([
-	function writeFileWithHeader (callback) {
-		// open file for writing, file is created (if it does not exist) or truncated (if it exists)
-		fs.writeFile(unifiedContract, config.get('solidity'), {flag: 'w'}, callback)
-	},
-	function readFiles (callback) {
-		async.concat(contracts, readFile, callback)
-	},
-	function clearFilesBeforeAppend (files, callback) {
-		async.map(files, removeStuff, callback)
-	},
-	function appendToUnified (clearedFiles, callback) {
-		async.each(clearedFiles, appendToFile, callback)
-	},
-], err => {
+unifyContracts(err => {
 	if(err) return console.error('err =', err)
 	console.log('DONE')
 })
 
-function readFile(file, cb) {
-	fs.readFile(__dirname + '/../../contracts/' + file, 'utf8', cb)
+function unifyContracts(callback) {
+	async.waterfall([
+		function writeFileWithHeader (callback) {
+			// open file for writing, file is created (if it does not exist) or truncated (if it exists)
+			fs.writeFile(unifiedContract, config.get('solidity'), {flag: 'w'}, callback)
+		},
+		function readFiles (callback) {
+			async.concat(contracts, readFile, callback)
+		},
+		function clearFilesBeforeAppend (files, callback) {
+			async.map(files, removeStuff, callback)
+		},
+		function appendToUnified (clearedFiles, callback) {
+			async.eachSeries(clearedFiles, appendToFile, callback)
+		},
+	], callback)
+
+	function readFile(file, cb) {
+		fs.readFile(__dirname + '/../../contracts/' + file, 'utf8', cb)
+	}
+
+	function removeStuff(file, cb) {
+		var cleared = file.replace(config.get('solidity'), '')
+		contracts.forEach(contract => {
+			cleared = cleared.replace('import \'./' + contract + '\';', '')
+		})
+		cb(null, cleared)
+	}
+
+	function appendToFile(file, cb) {
+		fs.appendFile(unifiedContract, file, cb)
+	}
 }
 
-function removeStuff(file, cb) {
-	var cleared = file.replace(config.get('solidity'), '')
-	contracts.forEach(contract => {
-		cleared = cleared.replace('import \'./' + contract + '\';', '')
-	})
-	cb(null, cleared)
-}
-
-function appendToFile(file, cb) {
-	fs.appendFile(unifiedContract, file, cb)
+module.exports = {
+	unify: unifyContracts
 }
