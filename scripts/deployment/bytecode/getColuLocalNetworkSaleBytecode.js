f7f7e5225d14b8d61a16011f5068d2a73e41faf9
Lior Rabin <lior@colu.com>
2017-12-20 14:51:48 +0200

    [ERC223] fixed deployment scripts

--- scripts/deployment/bytecode/getColuLocalNetworkSaleBytecode.js
+++ scripts/deployment/bytecode/getColuLocalNetworkSaleBytecode.js
@@ -28,11 +28,16 @@ web3.eth.getBlockNumber(function(err, lastBlock) {
       'Ownable.sol': fs.readFileSync(__dirname + '/../../../contracts/Ownable.sol', 'utf8'),
       'SafeMath.sol': fs.readFileSync(__dirname + '/../../../contracts/SafeMath.sol', 'utf8'),
       'ERC20.sol': fs.readFileSync(__dirname + '/../../../contracts/ERC20.sol', 'utf8'),
+      'ERC223.sol': fs.readFileSync(__dirname + '/../../../contracts/ERC223.sol', 'utf8'),
+      'ERC223Receiver.sol': fs.readFileSync(__dirname + '/../../../contracts/ERC223Receiver.sol', 'utf8'),
       'BasicToken.sol': fs.readFileSync(__dirname + '/../../../contracts/BasicToken.sol', 'utf8'),
+      'Standard223Token.sol': fs.readFileSync(__dirname + '/../../../contracts/Standard223Token.sol', 'utf8'),
       'TokenHolder.sol': fs.readFileSync(__dirname + '/../../../contracts/TokenHolder.sol', 'utf8'),
       'ColuLocalNetwork.sol': fs.readFileSync(__dirname + '/../../../contracts/ColuLocalNetwork.sol', 'utf8'),
+      'Standard223Receiver.sol': fs.readFileSync(__dirname + '/../../../contracts/Standard223Receiver.sol', 'utf8'),
+      'TokenOwnable.sol': fs.readFileSync(__dirname + '/../../../contracts/TokenOwnable.sol', 'utf8'),
       'VestingTrustee.sol': fs.readFileSync(__dirname + '/../../../contracts/VestingTrustee.sol', 'utf8'),
-      'ColuLocalNetworkSale.sol': fs.readFileSync(__dirname + '/../../../contracts/ColuLocalNetworkSale.sol', 'utf8'),
+      'ColuLocalNetworkSale.sol': fs.readFileSync(__dirname + '/../../../contracts/ColuLocalNetworkSale.sol', 'utf8')
     }
 
     solc.loadRemoteVersion(compilerVersion, function(err, solcSnapshot) {
@@ -49,7 +54,7 @@ web3.eth.getBlockNumber(function(err, lastBlock) {
 
       var result = '0x' + bytecode + params
 
-      var filePath = __dirname + '/../output/ColuLocalNetworkSaleBytecode_' + compilerVersion + '_' + now + '.txt'
+      var filePath = __dirname + '/../output/ColuLocalNetworkSale_' + compilerVersion + '_' + now + '.txt'
       fs.writeFile(filePath, result, {flag: 'w'}, function(err) {
         if(err) return console.error('err =', err)
         console.log('bytecode created at path =', filePath)
