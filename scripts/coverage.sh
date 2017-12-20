97f922b62006b84880e1c727da33eaae9b9d9725
Lior Rabin <lior@colu.com>
2017-12-20 14:52:05 +0200

    [ganache-cli] fixed bash scripts

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -9,6 +9,6 @@ port=8555
 trap cleanup EXIT
 
 echo "Starting testrpc-sc to generate coverage"
-eval ./node_modules/.bin/testrpc-sc --port $port --gasLimit 0xfffffffffff "$accounts" -u 0 -u 1 & 
+eval ./node_modules/.bin/testrpc-sc --port $port --gasLimit 0xfffffffffff "$accounts" -u 0 -u 1 &
 
 SOLIDITY_COVERAGE=true ./node_modules/.bin/solidity-coverage.cmd
