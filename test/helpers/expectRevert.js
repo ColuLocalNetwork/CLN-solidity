3ac6115ccf43ef78a417985a24052a0977972bab
Lior Rabin <lior@colu.com>
2017-12-21 16:54:01 +0200

    testrpc to ganche-cli updates

--- test/helpers/expectRevert.js
+++ test/helpers/expectRevert.js
@@ -6,7 +6,7 @@ module.exports = async (promise) => {
         const invalidOpcode = error.message.search('invalid opcode') > -1;
 
         // TODO: When we contract A calls contract B, and B throws, instead of an 'invalid jump', we get an 'out of gas'
-        // error. How do we distinguish this from an actual out of gas event? The testrpc log actually show an "invalid
+        // error. How do we distinguish this from an actual out of gas event? The ganache-cli log actually show an "invalid
         // jump" event).
         const outOfGas = error.message.search('out of gas') > -1;
 
