6d0dd83b4403d9a73a258ede9d7892b20567819b
Tal Beja <tal@colu.com>
2017-12-27 16:03:08 +0200

    update market maker interface

--- contracts/MarketMaker.sol
+++ contracts/MarketMaker.sol
@@ -3,12 +3,18 @@ pragma solidity 0.4.18;
 import './ERC223Receiver.sol';
 
 contract MarketMaker is ERC223Receiver {
-    function getPrice(address _fromToken, address _toToken) public constant returns (uint _price);
-    function changeAllowance(address _fromToken, address _toToken, uint _amount) public returns (uint _returnAmount);
-    function changeAllowance(address _fromToken, address _toToken, uint _amount, uint _minReturn) public returns (uint _returnAmount);
-    function change223(address _fromToken, address _toToken, uint _amount) public returns (uint _returnAmount);
-    function change223(address _fromToken, address _toToken, uint _amount, uint _minReturn) public returns (uint _returnAmount);
-    function quote(address _fromToken, address _toToken, uint _amount) public constant returns (uint _returnAmount);
+  uint public R1;
+  uint public R2;
+  uint public S1;
+  uint public S2;
+  uint public precision;
 
-    event Change(address indexed fromToken, uint inAmount, address indexed toToken, uint returnAmount, address indexed account);
+  function getPrice(address _fromToken, address _toToken) public constant returns (uint _price);
+  function changeAllowance(address _fromToken, address _toToken, uint _amount) public returns (uint _returnAmount);
+  function changeAllowance(address _fromToken, address _toToken, uint _amount, uint _minReturn) public returns (uint _returnAmount);
+  function change223(address _fromToken, address _toToken, uint _amount) public returns (uint _returnAmount);
+  function change223(address _fromToken, address _toToken, uint _amount, uint _minReturn) public returns (uint _returnAmount);
+  function quote(address _fromToken, address _toToken, uint _amount) public constant returns (uint _returnAmount);
+
+  event Change(address indexed fromToken, uint inAmount, address indexed toToken, uint returnAmount, address indexed account);
 }
