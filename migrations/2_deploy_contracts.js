b7b9eabf3759cdafc924d82088c71554063d5772
Leon <leon@leons-MacBook-Pro-work.local>
2018-01-30 12:50:53 +0200

    adding tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,20 @@
 var SafeMath = artifacts.require("./SafeMath.sol");
 var Ownable = artifacts.require("./Ownable.sol");
+var ERC20 = artifacts.require("./ERC20.sol");
+var BasicToken = artifacts.require("./BasicToken.sol");
 
 var ColuLocalNetwork = artifacts.require('./ColuLocalNetwork.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(SafeMath);
   deployer.deploy(Ownable);
+  //deployer.deploy(ERC20);
 
   deployer.link(SafeMath, ColuLocalNetwork);
   deployer.link(Ownable, ColuLocalNetwork);
+  //deployer.link(ERC20, BasicToken);
 
   deployer.deploy(ColuLocalNetwork);
+  deployer.deploy(BasicToken);
 };
 
