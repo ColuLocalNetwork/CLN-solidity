def46b79ff1d310c39b5fafc970e42987ad9c285
leonprou <leonp@colu.com>
2018-01-31 11:51:37 +0200

    delete uunnecesery

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
 var SafeMath = artifacts.require("./SafeMath.sol");
 var Ownable = artifacts.require("./Ownable.sol");
-var ERC20 = artifacts.require("./ERC20.sol");
 var BasicToken = artifacts.require("./BasicToken.sol");
 
 var ColuLocalNetwork = artifacts.require('./ColuLocalNetwork.sol');
