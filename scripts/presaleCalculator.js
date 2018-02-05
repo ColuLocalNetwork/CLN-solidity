e0321a1a086ecf40c236fb059638fa067b04224f
Lior Rabin <lior@colu.com>
2018-02-05 18:30:45 +0200

    updated precisions

--- scripts/presaleCalculator.js
+++ scripts/presaleCalculator.js
@@ -2,7 +2,7 @@ const BigNumber = require('bignumber.js');
 
 module.exports = (_tokensPerEth) => {
   const tokensPerEth = new BigNumber(_tokensPerEth);
-  const clnTokenUsdPriceSigDig = new BigNumber('95238095238095238095238095238095238');
+  const clnTokenUsdPriceSigDig = new BigNumber('95238095200000000');
   const decimals = new BigNumber(10 ** 18);
   return {
     calcPresale: (presales) => {
@@ -10,7 +10,7 @@ module.exports = (_tokensPerEth) => {
         let vestingPlanIndex = presale.plan.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
         let recipient = presale.recipient;
 
-        let tokensToReceive = new BigNumber(presale.dolarInvest).mul(decimals).mul(decimals).mul(decimals).div(clnTokenUsdPriceSigDig);
+        let tokensToReceive = new BigNumber(presale.dolarInvest).mul(decimals).mul(decimals).div(clnTokenUsdPriceSigDig);
         let etherValue = tokensToReceive.div(tokensPerEth).ceil();
 
         return [recipient, etherValue, vestingPlanIndex];
